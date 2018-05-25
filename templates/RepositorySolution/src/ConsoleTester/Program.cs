0f9fae20f66eb73bc04018ddd18780faf3c9c024
Brad R. Marshall <17801260+IObsequious@users.noreply.github.com>
2018-05-24 21:14:17 -0500

    Initial Commit
    Initial Commit

---
+++ templates/RepositorySolution/src/ConsoleTester/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleTester
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ModeConCols();
+            // 
+            PressAnyKey();
+        }
+        private static void PressAnyKey()
+        {
+            Console.WriteLine();
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private static void ModeConCols()
+        {
+            Console.SetWindowSize(160, 50);
+        }
+    }
+}
